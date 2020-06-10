b4c02c5f3a387cb763edb280c887b2b9bc1508d0
Mystic Quest <l1skou@gmail.com>
2020-06-10 03:29:48 +0300

    misc fix

--- constructorPractice.cpp
+++ constructorPractice.cpp
@@ -15,20 +15,34 @@ public:
     int getXp() { return xp; }
 
     Player(string name_val = "none", int health_val = 0, int xp_val = 0)
-        : name{name_val}, health{health_val}, xp{xp_val} {}
+        : name{name_val}, health{health_val}, xp{xp_val} { cout << "\nCreating: " << name_val; }
 
     Player(const Player &s)
-        : name(s.name), health(s.health), xp(s.xp) {}
-    ~Player() {}
+        : name(s.name), health(s.health), xp(s.xp) { cout << "\nCopying: " << name; }
+    ~Player() { cout << "\nDestroying: " << name; }
 };
+
+void DisplayPlayer(Player p)
+{
+    cout << p.getName();
+}
 
 int main()
 {
-    Player bob;
-    Player bernard{"Bernard"};
+    //test blocks
     {
-        Player archimonde{"Archimonde", 100};
+        Player bob;
+        Player bernard{"Bernard"};
+        DisplayPlayer(bernard);
+    }
+    {
+        Player otinanai{"Otinanai", 100};
         Player DestroyerOfWorlds{"DOW", 500, 300};
+        DisplayPlayer(DestroyerOfWorlds);
+    }
+    {
+        Player test{"TEST", 3000, 2000};
+        Player testCopy{test};
     }
 
     int x;
